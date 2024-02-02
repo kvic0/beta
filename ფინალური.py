5afe204e579caf07e3df324454fe45a8deaab9a7
kvic0 <kvicoo69@gmail.com>
2024-02-02 20:27:42 +0400

    beta test

--- ფინალური.py
+++ ფინალური.py
@@ -2,3 +2,5 @@ a = open("kitxvebi.txt", "r", encoding="utf-8")
 for each in a.readlines():
     print(each, end="")
 a.close()  # Always close the file when you're done with it
+
+#_________________________________________________
